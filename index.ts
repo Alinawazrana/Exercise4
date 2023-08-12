97593970d37db4f2ee8fcdd0aee73aa8b0401759
Alinawaz <139918066+TypescriptAlinawaz@users.noreply.github.com>
2023-08-12 20:59:03 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+/*Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new."*/
+
+console.log('Mola Ali (A.S) once said, "Depend only on GOd and yourself." ')
+
+console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new." ')
